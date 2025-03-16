9eb6b09a9de4d0f055ea18d711cc9b645c954734
Sayantika Ghosh <sayantika.ghosh@insideiim.com>
2025-03-17 02:53:26 +0530

    Update: ide feedback

--- src/main/java/com/geotrip/authservice/configurations/SpringSecurityConfig.java
+++ src/main/java/com/geotrip/authservice/configurations/SpringSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -29,7 +30,7 @@ public class SpringSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(
                 authorizeRequests -> authorizeRequests
--- src/main/java/com/geotrip/authservice/controllers/TestController.java
+++ src/main/java/com/geotrip/authservice/controllers/TestController.java
@@ -1,7 +1,6 @@
 package com.geotrip.authservice.controllers;
 
 
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
